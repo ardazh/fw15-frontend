9608937c19703c7bd12105ac505f8af3205a13a9
ardazh <127732352+ardazh@users.noreply.github.com>
2023-05-12 22:32:48 +0700

    add formik to login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,41 +1,18 @@
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import { FcGoogle } from 'react-icons/fc'
 import { FaFacebook } from 'react-icons/fa'
 import { useNavigate } from 'react-router-dom'
 import { Formik } from 'formik'
 import React from 'react'
 import http from '../helpers/http'
-import * Yup from 'yup'
+import * as Yup from 'yup'
 
-const validate
+const validationSchema = Yup.object({
+    email: Yup.string().email('Email is Invalid'),
+    password: Yup.string().required('Password is Invalid')
+})
 
-const FormLogin = ({ values, errors, touched, handlechange, handleBlur, handleSubmit, isSubmitting }) => {
-    const navigate = useNavigate()
-    const [errorMessage, setErrorMessage] = React.useState('')
-    const [token, setToken] = React.useState('')
-    // const doLogin = async (event) => {
-    //     event.preventDefault()
-    //     setErrorMessage('')
-    //     try {
-    //         const { value: email } = event.target.email
-    //         const { value: password } = event.target.password
-    //         const body = new URLSearchParams({ email, password }).toString()
-    //         const { data } = await http().post('http://localhost:8888/auth/login', body)
-    //         window.localStorage.setItem('token', data.results.token)
-    //         setToken(data.results.token)
-    //     } catch (err) {
-    //         const message = err?.response?.data?.message
-    //         if (message) {
-    //             setErrorMessage(message)
-    //         }
-    //     }
-
-    // }
-    React.useEffect(() => {
-        if (token) {
-            navigate('/')
-        }
-    }, [token, navigate])
+const FormLogin = ({ values, errors, touched, handleChange, handleBlur, handleSubmit, isSubmitting, warningMessage }) => {
     return (
         <form onSubmit={handleSubmit} className="w-[80%] flex flex-col gap-5">
             <div>
@@ -47,24 +24,24 @@ const FormLogin = ({ values, errors, touched, handlechange, handleBlur, handleSu
             <div>
                 Hi, Welcome back to Urticket!
             </div>
-            {errorMessage &&
+            {warningMessage &&
                 (<div>
-                    <div className='alert alert-error'>{errorMessage}</div>
+                    <div className='alert alert-warning'>{warningMessage}</div>
                 </div>)}
             <div className='from-control'>
                 <input
                     placeholder="Email"
-                    className="input input-bordered w-full input-error"
+                    className={`input input-bordered w-full${errors.email && touched.email && "input-error"}`}
                     type="email"
                     name="email"
-                    onChange={handlechange}
+                    onChange={handleChange}
                     onBlur={handleBlur}
                     value={values.email}
                 />
-                {error.email && touched.email && (
+                {errors.email && touched.email && (
                     <label className='label'>
                         <span className='label-text-alt text-error'>
-                        {error.email}
+                            {errors.email}
                         </span>
                     </label>
                 )}
@@ -72,17 +49,17 @@ const FormLogin = ({ values, errors, touched, handlechange, handleBlur, handleSu
             <div className='from-control'>
                 <input
                     placeholder="password"
-                    className="input input-bordered w-full input-error"
+                    className={`input input-bordered w-full${errors.password && touched.password && "input-error"}`}
                     type="password"
                     name="password"
-                    onChange={handlechange}
+                    onChange={handleChange}
                     onBlur={handleBlur}
                     value={values.password}
                 />
-                {error.password && touched.password && (
+                {errors.password && touched.password && (
                     <label className='label'>
                         <span className='label-text-alt text-error'>
-                        {error.password}
+                            {errors.password}
                         </span>
                     </label>
                 )}
@@ -91,7 +68,7 @@ const FormLogin = ({ values, errors, touched, handlechange, handleBlur, handleSu
                 <Link className='text-primary fond-bold' to="/forgot-password">Forgot Password?</Link>
             </div>
             <div>
-                <button className="btn btn-primary btn-block">Sign In</button>
+                <button disabled={isSubmitting} type='submit' className="btn btn-primary btn-block">Sign In</button>
             </div>
             <div className='text-center'>
                 or signin with
@@ -109,22 +86,62 @@ const FormLogin = ({ values, errors, touched, handlechange, handleBlur, handleSu
 }
 
 const Login = () => {
+    const location = useLocation()
+    const navigate = useNavigate()
+    const [token, setToken] = React.useState('')
+    const [warningMessage, setWarningMessage] = React.useState(location.state?.warningMessage)
+    const [errorMessage, setErrorMessage] = React.useState('')
+    React.useEffect(() => {
+        if (token) {
+            navigate('/')
+        }
+    }, [token, navigate])
 
+    const doLogin = async (values, { setSubmitting, setErrors }) => {
+        setWarningMessage('')
+        setErrorMessage('')
+        try {
+            const { email, password } = values
+            const body = new URLSearchParams({ email, password }).toString()
+            const { data } = await http().post('http://localhost:8888/auth/login', body)
+            window.localStorage.setItem('token', data.results.token)
+            setSubmitting(false)
+            setToken(data.results.token)
+        } catch (err) {
+            const message = err?.response?.data?.message
+            if (message) {
+                if (err?.response?.data?.results) {
+                    setErrors({
+                        email: err.response.data.results.filter(item => item.param === "email")[0].message,
+                        password: err.response.data.results.filter(item => item.param === "password")[0].message,
+                    })
+                } else {
+                    setErrorMessage(message)
+                }
+            }
+        }
+    }
     return (
-        <div className="flex h-screen">
-            <div className="hidden md:flex flex-1 bg-orange-400"></div>
-            <div className="max-w-md w-full flex justify-center items-center">
-                <Formik initialValues={{
-                    email: '',
-                    password: ''
-                }}>
-                    {(proops) => {
-                        <FormLogin {...proops} />
-                    }}
-                </Formik>
+        <>
+            <div className="flex h-screen">
+                <div className="hidden md:flex flex-1 bg-orange-400"></div>
+                <div className="max-w-md w-full flex justify-center items-center">
+                    <Formik
+                        initialValues={{
+                            email: '',
+                            password: ''
+                        }}
+                        validationSchema={validationSchema}
+                        onSubmit={doLogin}
+                    >
+                        {(props) => (
+                            <FormLogin {...props} warningMessage={warningMessage} errorMessage={errorMessage} />
+                        )}
+                    </Formik>
+                </div>
             </div>
-        </div>
+        </>
     )
 }
 
-export default Login;
+export default Login
