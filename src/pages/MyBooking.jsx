a3a4420c4b2b249e1b0cf1141c623c348722a904
ardazh <rifaardan@gmail.com>
2023-07-31 15:09:39 +0700

    feat: maping data MyBooking

--- src/pages/MyBooking.jsx
+++ src/pages/MyBooking.jsx
@@ -1,76 +1,86 @@
 import Header from "../components/Header"
 import Footer from "../components/Footer"
+import Sidebar from "../components/Sidebar"
+import { useSelector } from "react-redux"
+import React from "react"
+import http from "../helpers/http"
+import { FiCalendar } from "react-icons/fi"
+import moment from "moment/moment"
 
 const MyBooking = () => {
-    return (
-        <>
-            <div>
-                <Header />
+  const [myBooking, setMyBooking] = React.useState([])
+  const token = useSelector(state => state.auth.token)
+
+  React.useEffect(() => {
+    async function getMyEvent(){
+      const {data} = await http(token).get("/history?limit=3")
+      setMyBooking(data.results)
+    }
+    if(token){
+      getMyEvent()
+    }
+  }, [token, setMyBooking])
+  console.log(myBooking)
+  return (
+    <>
+      <div>
+        <Header />
+      </div>
+      <div className='flex'>
+        <Sidebar />
+        <main className='flex-1 my-12 text-sm'>
+          <div className='bg-gray-400 px-12 py-11 rounded-2xl mr-16 h-[750px]'>
+            <div className='flex justify-between'>
+              <div className='font-bold tracking-wider mb-12 text-xl'>My Booking</div>
+              <div className='flex justify-center items-center border-2 w-[125px] h-12 bg-[#FFE15D] rounded-xl gap-4 text-[#F49D1A]'>
+                <i>
+                  <FiCalendar size={25} />
+                </i>
+                  March
+              </div>
             </div>
-            <div className='flex'>
-                <aside className='hidden md:block w-60 mx-16 my-12'>
-                    <div className='flex gap-3.5 mb-14'>
-                        <div>
-                            <div className='inline-block rounded-full p-[2px] bg-gradient-to-r from-cyan-500 to-blue-500'>
-                                <img className='w-11 h-11 object-cover rounded-full border-2 border-white' src='/assets/profile1.jpg' alt='profile1' />
-                            </div>
-                        </div>
-                        <div>
-                            <div className='text-sm'>
-                                Kim Ji-soo
-                            </div>
-                            <div className='text-xs'>
-                                Entrepreneur, ID
-                            </div>
-                        </div>
+            {myBooking.map((booking) => {
+              return(
+                <>
+                  <div className='flex items-center justify-start gap-6 border-b-2 py-7'>
+                    <div>
+                      <div className='w-[50px] h-[75px] flex flex-col items-center justify-center rounded-2xl bg-white shadow-lg'>
+                        <div className='text-sm font-semibold text-[#FF8900]'>{moment(booking?.date).format("DD")}</div>
+                        <div className='text-xs font-medium text-[#C1C5D0]'>{moment(booking?.date).format("LLLL").slice(0, 3)}</div>
+                      </div>
                     </div>
-                    <ul>
-                        <li className='flex gap-7 mb-8 text-sm'><i data-feather='user'></i>Profile</li>
-                        <li>
-                            <ul className='ml-7'>
-                                <li className='flex gap-7 mb-8 text-sm'><i data-feather='credit-card'></i>Card</li>
-                                <a href='/profile-edit-profile.html'>
-                                    <li className='flex gap-7 mb-8 text-sm'><i data-feather='edit-3'></i>Edit Profile</li>
-                                </a>
-                                <a href='/profile-change-password.html'>
-                                    <li className='flex gap-7 mb-8 text-sm'><i data-feather='unlock'></i>Change Password</li>
-                                </a>
-                            </ul>
-                        </li>
-                        <a href='/my-booking.html'>
-                            <li className='flex gap-7 mb-8 text-sm'><i data-feather='list'></i>My Booking</li>
-                        </a>
-                        <a href='/my-wishlist.html'>
-                            <li className='flex gap-7 mb-8 text-sm'><i data-feather='heart'></i>My Wishlist</li>
-                        </a>
-                        <li className='flex gap-7 mb-8 text-sm'><i data-feather='settings'></i>Settings</li>
-                        <li className='flex gap-7 mb-8 text-sm text-[#F03800]'><i data-feather='log-out'></i>Logout</li>
-                    </ul>
-                </aside>
-                <main className='flex-1 my-12 text-sm'>
-                    <div className='bg-white px-12 py-11 rounded-2xl mr-16 h-[750px]'>
-                        <div className='flex justify-between'>
-                            <div className='text-xl font-bold tracking-wider mb-12 text-xl'>My Booking</div>
-                            <div className='flex justify-center items-center border-2 w-[125px] h-12 bg-[#FFE15D] rounded-xl gap-4 text-[#F49D1A]'><i data-feather='calendar'></i>March</div>
-                        </div>
-                        <div className='flex justify-center items-center'>
-                            <div className='flex flex-col justify-center items-center mt-60'>
-                                <div className='text-xl tracking-[1px]'>
-                                    No tickets bought
-                                </div>
-                                <div className='flex justify-center text-center text-sm text-[#B3B8B8] w-80 h-[62px] tracking-[0.5px] gap-5'>
-                                    It appears you haven’t bought any tickets yet. Maybe try searching these?
-                                </div>
-                            </div>
-                        </div>
+                    <div className='flex flex-col items-start justify-start text-[#373A42] gap-[5px]'>
+                      <div className='text-2xl font-semibold tracking-[2px] mb-3.5 capitalize'>{booking?.title}</div>
+                      <div className='text-xs tracking-[0.5px] capitalize'>{booking?.location}, Indonesia</div>
+                      <div className='text-xs tracking-[0.5px]'>{moment(booking?.date).format("LLLL")}</div>
+                      <div className='text-xs traacking-[0.5px] text-[#3366FF]'>
+                        <a href='#'>Detail</a>
+                      </div>
                     </div>
-                </main>
+                  </div>
+                </>
+              )
+            })}
+            <div className='flex justify-center items-center'>
+              {myBooking.length < 1 && (
+                <div className='flex flex-col justify-center items-center mt-60'>
+                  <div className='text-xl tracking-[1px]'>
+                  No tickets bought
+                  </div>
+                  <div className='flex justify-center text-center text-sm text-[#B3B8B8] w-80 h-[62px] tracking-[0.5px] gap-5'>
+                  It appears you haven&apos;t bought any tickets yet. Maybe try searching these?
+                  </div>
+                </div>
+              )}
             </div>
-            <div>
-                <Footer />
-            </div>
-        </>
-    )
+          </div>
+        </main>
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  )
 }
 
 export default MyBooking
